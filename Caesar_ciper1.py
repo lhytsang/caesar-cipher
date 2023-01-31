df10d00be6c5a0fccf29d01a326f90ea7a870504
LTsang01 <73853496+LTsang01@users.noreply.github.com>
2023-01-31 04:00:28 +0000

    Creating alphabet variable and splitting it into individual characters

--- Caesar_ciper1.py
+++ Caesar_ciper1.py
@@ -6,7 +6,8 @@ Created on Tue Jan 31 02:54:19 2023
 """
 
 fileText = []
-
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letters = list(alphabet)
 myFile = open("ciphertext1-2.txt", "r")
 for line in myFile:     
     line = line.strip()
